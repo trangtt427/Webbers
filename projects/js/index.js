82bad4151fd62248c3e9d4d6f5239cde340de977
Trvngwreck <trangtt427@gmail.com>
2017-10-30 16:01:18 -0700

    yae
    yea

---
+++ projects/js/index.js
@@ -0,0 +1,85 @@
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+$('.nav-item').click(function(){
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');                     
+});
+
+
+
+
+
+$(document).scroll(function() {
+  var st = $(this).scrollTop();
+  $("#header").css({
+    "background-position-y": (-st/20)
+  })
+  $("#first-section").css({
+    "top": (-st/5),
+    "bottom": (st/5)
+  })
+  
+});
+
+
+
+
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 500);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('page', true);
+    show('loading', false);
+});
+
