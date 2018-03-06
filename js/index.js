3260a47ec1d00eee2acf4e4c516b649e01daf1f5
Trang Tran <trangtran@Trangs-MacBook-Pro.local>
2018-03-05 21:39:49 -0800

    changed a lot
    new web

--- js/index.js
+++ js/index.js
@@ -1,85 +1,37 @@
 
 
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('header').outerHeight();
+    
+ 
+/*parallax*/
 
-$(window).scroll(function(event){
-    didScroll = true;
+//when the window is scrolling, do stuff
+$(window).scroll(function() {
+  //capture the curren position of the window
+  var windowTop = $(window).scrollTop();
+  //make the H1 drift up and get more transparent 
+  $("header h1").css({
+    "bottom": windowTop * 1,
+    "opacity": 1 - (windowTop/275)
+  });
+  //shift the header background slowly. try a negative windowTop or multiply instead of divide
+  $("header").css("background-position-y", windowTop / 3);
+  console.log(windowTop);
 });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
 
-function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('header').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav-up').addClass('nav-down');
+
+  //scroollll top
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var top =  $(".goto-top");
+        if ( $('body').height() <= (    $(window).height() + $(window).scrollTop() + 200 )) {
+  top.animate({"margin-left": "0px"},1500);
+        } else {
+            top.animate({"margin-left": "0px"},1500);
         }
-    }
-    
-    lastScrollTop = st;
-}
+    });
 
-$('.nav-item').click(function(){
-    $(this).siblings().removeClass('active');
-    $(this).addClass('active');                     
+    $(".goto-top").on('click', function () {
+        $("html, body").animate({scrollTop: 0}, 1100);
+    });
 });
-
-
-
-
-
-$(document).scroll(function() {
-  var st = $(this).scrollTop();
-  $("#header").css({
-    "background-position-y": (-st/20)
-  })
-  $("#first-section").css({
-    "top": (-st/5),
-    "bottom": (st/5)
-  })
-  
-});
-
-
-
-
-function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 500);
-
-    function checkReady() {
-        if (document.getElementsByTagName('body')[0] !== undefined) {
-            window.clearInterval(intervalID);
-            callback.call(this);
-        }
-    }
-}
-
-function show(id, value) {
-    document.getElementById(id).style.display = value ? 'block' : 'none';
-}
-
-onReady(function () {
-    show('page', true);
-    show('loading', false);
-});
-
