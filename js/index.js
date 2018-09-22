1b5978393f3468ab3a888e86e0fee59285603bc4
Trang Tran <trangtran@Trangs-MacBook-Pro.local>
2018-09-21 17:51:30 -0700

    Update Website
    Updated Website

--- js/index.js
+++ js/index.js
@@ -1,50 +1,47 @@
 
 
-    
- 
-/*parallax*/
-
-//when the window is scrolling, do stuff
-$(window).scroll(function() {
-  //capture the curren position of the window
-  var windowTop = $(window).scrollTop();
-  //make the H1 drift up and get more transparent 
-  $("header h1").css({
-    "bottom": windowTop * 1,
-    "opacity": 1 - (windowTop/175)
-  });
-  //shift the header background slowly. try a negative windowTop or multiply instead of divide
-  $("header").css("background-position-y", windowTop / 3);
-  console.log(windowTop);
-});
-
-
 
   //scroollll top
 $(document).ready(function () {
   $(window).scroll(function () {
     var top =  $(".goto-top");
         if ( $('body').height() <= (    $(window).height() + $(window).scrollTop() + 200 )) {
-  top.animate({"margin-left": "0px"},1200);
+  top.animate({"margin-left": "0px"},1500);
         } else {
-            top.animate({"margin-left": "0px"},1200);
+            top.animate({"margin-left": "0px"},1500);
         }
     });
 
     $(".goto-top").on('click', function () {
-        $("html, body").animate({scrollTop: 0}, 1200);
+        $("html, body").animate({scrollTop: 0}, 1100);
     });
 });
 
 
 
-//scrollanchor
 
-$(function() {
-  $('#up').click(function(){
-    $('html, body').animate({
-        scrollTop: $("#down").offset().top
-    }, 1200);
-});
+
+
+
+
+
+$(document).scroll(function() {
+  var st = $(this).scrollTop();
+  $("#header").css({
+    "background-position-y": (-st/20)
+  })
+  $("#first-section").css({
+    "top": (-st/5),
+    "bottom": (st/5)
+  })
   
- })
+});
+
+
+
+
+
+
+
+
+
