bbec06b5a4882407e39e0174fa4faf79e2119b2f
Trang Tran <trangtran@Trangs-MacBook-Pro.local>
2018-10-03 16:54:41 -0700

    optimize

--- js/index.js
+++ js/index.js
@@ -119,3 +119,22 @@ $(document).ready(function () {
 		// Animate loader off screen
 		$(".se-pre-con").fadeOut("slow");;
 	});
+
+//replace blurry images
+(() => {
+  'use strict';
+  // Page is loaded
+  const objects = document.getElementsByClassName('asyncImage');
+  Array.from(objects).map((item) => {
+    // Start loading image
+    const img = new Image();
+    img.src = item.dataset.src;
+    // Once image is loaded replace the src of the HTML element
+    img.onload = () => {
+      item.classList.remove('asyncImage');
+      return item.nodeName === 'IMG' ? 
+        item.src = item.dataset.src :        
+        item.style.backgroundImage = `url(${item.dataset.src})`;
+    };
+  });
+})();
