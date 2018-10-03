0012d7b8312c277281df4d8c3b340b9683bc9d6a
Trang Tran <trangtran@Trangs-MacBook-Pro.local>
2018-10-03 12:45:55 -0700

    added in scroll transition

--- js/index.js
+++ js/index.js
@@ -89,6 +89,27 @@ $(document).ready(function () {
 
 
 
+// Fade bio pictures in
+  function showImages(el) {
+        var windowHeight = jQuery( window ).height();
+        $(el).each(function(){
+            var thisPos = $(this).offset().top;
 
+            var topOfWindow = $(window).scrollTop();
+            if (topOfWindow + windowHeight - 200 > thisPos ) {
+                $(this).addClass("fadeIn");
+            }
+        });
+    }
+
+    // if the image in the window of browser when the page is loaded, show that image
+    $(document).ready(function(){
+            showImages('.star');
+    });
+
+    // if the image in the window of browser when scrolling the page, show that image
+    $(window).scroll(function() {
+            showImages('.star');
+    });
 
 
