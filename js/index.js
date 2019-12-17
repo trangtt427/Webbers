2e12b6b4abf2396cae6b2e0358cf5f8869f11b23
Trang Tran <trangtran@Trangs-MacBook-Pro-2.local>
2019-12-17 11:28:38 -0800

    test

--- js/index.js
+++ js/index.js
@@ -24,23 +24,50 @@ $(document).ready(function() {
   });
 
   // Hide Header on on scroll down
-  $(function() {
-    var lastScrollTop = 0,
-      delta = 15;
-    $(window).scroll(function(event) {
-      var st = $(this).scrollTop();
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $("header").outerHeight();
 
-      if (Math.abs(lastScrollTop - st) <= delta) return;
-      if (st > lastScrollTop && lastScrollTop > 0) {
-        // downscroll code
-        $("header").css("top", "-80px");
-      } else {
-        // upscroll code
-        $("header").css("top", "0px");
-      }
-      lastScrollTop = st;
-    });
+  $(window).scroll(function(event) {
+    didScroll = true;
   });
+
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+      // Scroll Down
+      $("header")
+        .removeClass("nav-down")
+        .addClass("nav-up");
+    } else {
+      // Scroll Up
+      if (st + $(window).height() < $(document).height()) {
+        $("header")
+          .removeClass("nav-up")
+          .addClass("nav-down");
+      }
+    }
+
+    lastScrollTop = st;
+  }
+
+  var $document = $(document),
+    $element = $("header"),
+    className = "box-shadow";
 
   // Start listening for the 'scroll' event
   $document.scroll(function() {
