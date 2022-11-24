c487348f1d34c8534444d7febd5a0a7bf406a066
Trang Tran <trangtran@Trangs-MacBook-Pro.local>
2022-11-23 18:52:20 -0800

    remove script

--- js/script.js
+++ js/script.js
@@ -147,15 +147,7 @@ $(document).ready(function () {
   }
 
 
-  // if the image in the window of browser when the page is loaded, show that image
-$(document).ready(function() {
-  showImages(".star");
-});
 
-// if the image in the window of browser when scrolling the page, show that image
-$(window).scroll(function() {
-  showImages(".star");
-});
 
   //on or scroll, detect elements in view
   $(window).on("scroll resize", function () {
