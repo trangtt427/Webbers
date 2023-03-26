ec27d354e3431aa72e688c789501cf5e1ec36c0a
Trvngwreck <trangtt427@gmail.com>
2023-03-26 03:13:23 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,59 +1,14 @@
 // thank you kimmy c:
 //yer welcmoe
 
-var $document = $(document),
-  $element = $("header"),
-  classNames = "box-shadow sticky";
-
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $("header").outerHeight();
-var firstSectionHeight = $("#first-section").outerHeight();
-
-// Start listening for the 'scroll' event
-$document.scroll(function (event) {
-  didScroll = true;
-
-  if ($document.scrollTop() >= 55) {
-    // Change 50 to the value you require
-    // for the event to trigger
-    $element.addClass(classNames);
-  } else {
-    $element.removeClass(classNames);
+$(window).scroll(function() {     
+  var scroll = $(window).scrollTop();
+  if (scroll > 100) {
+      $("header").addClass("box-shadow");
   }
-});
-
-setInterval(function () {
-  if (didScroll) {
-    hasScrolled();
-    didScroll = false;
+  else {
+      $("header").removeClass("box-shadow");
   }
-}, 250);
-
-function hasScrolled() {
-  var st = $(this).scrollTop();
-
-  // Make sure they scroll more than delta
-  if (Math.abs(lastScrollTop - st) <= delta) return;
-
-  // If they scrolled down and are past the navbar, add class .nav-up.
-  // This is necessary so you never see what is "behind" the navbar.
-  if (st > lastScrollTop && st > firstSectionHeight - navbarHeight) {
-    // Scroll Down
-    $("header").removeClass("nav-down").addClass("nav-up");
-  } else if (st + $(window).height() < $(document).height()) {
-    // Scroll Up
-    $("header").removeClass("nav-up").addClass("nav-down");
-  }
-
-  lastScrollTop = st;
-}
-
-$(".nav-item").click(function () {
-  $(this).siblings().removeClass("active");
-  $(this).addClass("active");
 });
 
 //hamburger
